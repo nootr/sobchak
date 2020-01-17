0d4d3fb3c17e05a2ce7c0781d3aa7cac7efbeeda
Joris Hartog <jhartog@hostnet.nl>
2020-01-17 09:25:43 +0100

    Bump to v0.3
    Changelog:
    
    * Bugfix: missing library has been added
    * Requirements list has been narrowed down
    * Generating a hypervisor plot will not change its state

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='sobchak',
-    version='0.2',
+    version='0.3',
     description='OpenStack instance scheduling optimizer',
     license='MIT',
     long_description=long_description,
